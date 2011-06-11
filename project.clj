edfa62485220e92ec54d3e2314b82abd139bd5c9
George Rogers <geo@myhost.(none)>
2011-06-11 07:26:48 -0500

    foo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject my-blog "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [hiccup "0.3.5"]
                  [compojure "0.6.3"]
