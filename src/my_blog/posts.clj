ace5eaae69cd15be313b1864acf611b74d570c09
George Rogers <geo@localhost.localdomain>
2012-06-30 17:51:44 -0400

    foo

--- src/my_blog/posts.clj
+++ src/my_blog/posts.clj
@@ -1,14 +1,11 @@
 (ns my-blog.posts
   (:import [com.petebevin.markdown MarkdownProcessor])
   (:require [clojure.string :as string]
-            [clojure.java.jdbc :as sql]))
+            [clojure.java.jdbc :as sql]
+            [my-blog.users :as users]))
 (def db (System/getenv "DATABASE_URL"))
 (defn markdown [mdown]
   (. (MarkdownProcessor.) markdown mdown))
-(defn read-lines []
-  (lazy-seq
-   (when-let [line (read-line)]
-     (cons line (read-lines)))))
 (defn extract-metadata [html]
   (try 
     (with-in-str (slurp html)
@@ -39,7 +36,14 @@
     (sql/do-commands
      "CREATE TABLE posts(title char(80) primary key,
                          updated timestamp not null,
-                         content text not null)")))
+                         content text not null)"
+     "CREATE TABLE users(username char(80) primary key,
+                         password varchar(255) not null)")
+    (let [_ (print "new username: ")
+          username (read-line)
+          _ (print "new password: ")
+          password (read-line)]
+      (users/create-user username password))))
 (defn post [title]
   (sql/with-connection db
     (sql/with-query-results rows
